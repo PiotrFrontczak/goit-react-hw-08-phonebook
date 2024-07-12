9bcf020a501c6401c4a303c4c1319cdeafb61911
Piotr Frontczak <piotrf2023@gmail.com>
2024-07-12 19:29:01 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts, deleteContact } from '../redux/slices/contactsSlice';
+import { fetchContacts } from '../redux/slices/contactsSlice';
 import ContactList from './ContactList';
 import AddContactForm from './AddContactForm';
 import styles from '../components/Contacts.module.scss';
-import PropTypes from 'prop-types';
 
-
-const AddContact = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector((state) => state.contacts);
-  const [name, setName] = React.useState('');
-  const [number, setNumber] = React.useState('');
-
-  const handleChange = (e) => {
-    const { name, value } = e.currentTarget;
-    if (name === 'name') setName(value);
-    if (name === 'number') setNumber(value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (contacts.some(contact => contact.name === name)) {
-      alert("Contact with this name already exists.");
-      return;
-    }
-
-    dispatch(addContact(name, number));
-    setName('');
-    setNumber('');
-  };
-
-};
-
-AddContactForm.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
 
 const App = () => {
   const dispatch = useDispatch();
-  const { contacts, status, error } = useSelector(state => state.contacts);
+  const { status, error } = useSelector(state => state.contacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
-
-  useEffect(() => {
-    console.log('Contacts state in App:', contacts);
-  }, [contacts]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -64,23 +22,11 @@ const App = () => {
     return <div>Error: {error}</div>;
   }
 
-  if (!Array.isArray(contacts)) {
-    console.error('Contacts is not an array:', contacts);
-    return <div>Error: Contacts is not an array</div>;
-  }
-
   return (
-    <div>
-      <h1>Phonebook</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Phonebook</h1>
       <AddContactForm />
-      <ul>
-        {contacts.filter(contact => contact).map(contact => (
-          <li key={contact.id}>
-            {contact.name}: {contact.phone}
-            <button onClick={() => dispatch(deleteContact(contact.id))}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <ContactList />
     </div>
   );
 };
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts, deleteContact } from '../redux/slices/contactsSlice';
+import { deleteContact } from '../redux/slices/contactsSlice';
 import styles from './Contacts.module.scss';
+
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const { contacts, status, error } = useSelector(state => state.contacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -24,7 +21,12 @@ const ContactList = () => {
       {contacts.map(contact => (
         <li key={contact.id} className={styles.listItem}>
           <span>{contact.name}: {contact.phone}</span>
-          <button onClick={() => dispatch(deleteContact(contact.id))} className={styles.deleteButton}>Delete</button>
+          <button
+            onClick={() => dispatch(deleteContact(contact.id))}
+            className={styles.deleteButton}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
