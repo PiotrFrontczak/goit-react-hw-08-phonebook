cccff09e664dd2ceb0123afc44ea1c00378212a0
Piotr Frontczak <piotrf2023@gmail.com>
2024-07-12 19:01:39 +0200

    Update AddContactForm.jsx

--- src/components/AddContactForm.jsx
+++ src/components/AddContactForm.jsx
@@ -1,39 +1,56 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../redux/slices/contactsSlice';
 import styles from './Contacts.module.scss';
 
 const AddContactForm = () => {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
   const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.contacts);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addContact({ name, phone }));
+
+    if (contacts.some(contact => contact.name === name)) {
+      alert("Contact with this name already exists.");
+      return;
+    }
+
+    dispatch(addContact({ name, phone: number }));
     setName('');
-    setPhone('');
+    setNumber('');
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label>Name</label>
       <input
         type="text"
+        name="name"
+        pattern="^[A-Za-z]+(\s[A-Za-z]+){0,2}$"
+        required
         value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
+        onChange={handleChange}
         className={styles.input}
-        required
       />
+      <label>Phone number</label>
       <input
-        type="text"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-        placeholder="Phone"
-        className={styles.input}
+        type="tel"
+        name="number"
+        pattern="^\d{9}$"
         required
+        value={number}
+        onChange={handleChange}
+        className={styles.input}
       />
-      <button type="submit" className={styles.button}>Add Contact</button>
+      <button type="submit" className={styles.button}>Add contact</button>
     </form>
   );
 };
