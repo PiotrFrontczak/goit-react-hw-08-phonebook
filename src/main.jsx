e01a273c75f6e57745618be8f4f663550db83b6a
Piotr Frontczak <piotrf2023@gmail.com>
2024-07-13 23:07:22 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const theme = createTheme({
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <Router>
+    <Router basename="/goit-react-hw-08-phonebook">
       <div>
         <Navigation />
         <UserMenu />
